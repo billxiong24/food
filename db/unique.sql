acc064ee2bd8380e9090ec0b6bbb7cb1f10a81ab
billxiong24 <billx0477@gmail.com>
2019-02-06 02:51:53 -0500

    fix unique sequencer users

--- db/unique.sql
+++ db/unique.sql
@@ -27,3 +27,16 @@ END LOOP;
 END
 $func$  LANGUAGE plpgsql;
 ALTER TABLE ingredients ALTER COLUMN num SET DEFAULT unique_ingred_num_seq();
+
+CREATE OR REPLACE FUNCTION unique_users_num_seq(OUT nextfree bigint) AS
+$func$
+BEGIN
+LOOP
+   SELECT INTO nextfree val
+   FROM   nextval('users_id_seq'::regclass) val
+   WHERE  NOT EXISTS (SELECT 1 FROM users WHERE id = val);
+   EXIT WHEN FOUND;
+END LOOP; 
+END
+$func$  LANGUAGE plpgsql;
+ALTER TABLE users ALTER COLUMN id SET DEFAULT unique_users_num_seq();
