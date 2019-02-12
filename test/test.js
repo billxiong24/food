aa62688e17eda1dfa4503ed08df6c6e9d999faec
billxiong24 <billx0477@gmail.com>
2019-02-12 02:32:04 -0500

    cjamges

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const execSync = require('child_process').execSync;
-execSync("./cleandb.sh");
+//execSync("./cleandb.sh");
 require('./test_sku');
 require('./test_product_lines');
 require('./test_manufacturing_goals');
