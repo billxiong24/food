0a9e281e0c650fd9a378c686f7367f590391be1e
gohuynh <gordon.huynh@gmail.com>
2019-02-19 15:03:23 -0500

    typo

--- routes/guard.js
+++ routes/guard.js
@@ -1,7 +1,7 @@
 const checkUser = (req, res, next) => {
   let whitelist = false;
   if(req.path.search(/^\/users\/*/) >= 0) whitelist = true;
-  
+
   if(!whitelist && (!req.session.user || !req.sessionID)) {
     res.status(401).json({
       error: "Please log in first"
@@ -12,7 +12,7 @@ const checkUser = (req, res, next) => {
 }
 
 const checkAdmin = (req, res, next) => {
-  let whitelist = false,
+  let whitelist = false;
   if(req.path.search(/^\/users\/*/) >= 0) whitelist = true;
   else if(req.path.search(/^\/manufacturing_goals\/*/)) whitelist = true;
 
