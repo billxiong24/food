663348845e77789eff5d98c654dd4a9b9909531b
gohuynh <gordon.huynh@gmail.com>
2019-02-19 19:01:48 -0500

    CORS sucks

--- app.js
+++ app.js
@@ -28,8 +28,9 @@ var app = express();
 app.use(cors({
   credentials: true,
   origin: function(origin, callback) {
-    console.log(origin);
+    console.log(origin === 'http://localhost/3000');
     if (origin === domain || origin === 'http://localhost/3000') {
+      console.log(origin);
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
