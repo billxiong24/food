0627774c58630e8f181bb18ddca3032658a4722b
rsh25@duke.edu <rsh25@duke.edu>
2019-02-26 01:11:10 -0500

    g

--- app/scheduler.js
+++ app/scheduler.js
@@ -50,13 +50,42 @@ class Scheduler extends CRUD {
 
     get_goals(){
         let goals = []
-        let query = squel.select()
-        .from(this.manufacturing_goal_sku)
-        .where("start_time != ?", 0)
-        .where("end_time != ?", 0)
-        .where("man_line_id != ?", 0)
-        .toString();
-        return db.execSingleQuery(query, []);
+        let query = `SELECT
+        manufacturing_goal_sku.mg_id,
+        manufacturing_goal_sku.sku_id,
+        manufacturing_goal_sku.quantity,
+        manufacturing_goal_sku.start_time,
+        manufacturing_goal_sku.end_time,
+        manufacturing_goal_sku.man_line_id,
+        manufacturing_goal.name,
+        manufacturing_goal.user_id,
+        manufacturing_goal.deadline,
+        enabled,
+        manufacturing_goal.name,
+        sku.num,
+        sku.case_upc,
+        sku.unit_upc,
+        sku.unit_size,
+        sku.count_per_case,
+        sku.prd_line,
+        sku.comments,
+        sku.formula_id,
+        sku.formula_scale,
+        sku.man_rate
+        FROM manufacturing_goal_sku 
+        INNER JOIN manufacturing_goal on manufacturing_goal_sku.mg_id= manufacturing_goal.id  
+        INNER JOIN sku ON manufacturing_goal_sku.sku_id=sku.id 
+        WHERE mg_id IN
+        (
+        SELECT mg_id
+        FROM manufacturing_goal_sku
+        WHERE start_time!=0 AND end_time!=0 AND man_line_id!=0
+        )`
+        db.execSingleQuery(query, [])
+        .then(function(res){
+            console.log(res)
+            return res
+        })
     }
 
     get_man_lines(){
