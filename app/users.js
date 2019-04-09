adc6db7f472829534d587b2ba45dc0cb66deb405
gohuynh <gordon.huynh@gmail.com>
2019-04-09 18:35:19 -0400

    WIP rbac

--- app/users.js
+++ app/users.js
@@ -7,126 +7,157 @@ const QueryGenerator = require("./query_generator");
 const saltRounds = 10;
 
 class Users extends CRUD {
-    constructor() {
-        super();
-        this.tableName = "users";
-    }
-
-    checkExisting(dataObj) {
-        if(!dataObj.uname) {
-            return Promise.reject("Bad Username.");
-        }
-
-        let query = "SELECT COUNT(*) FROM " + this.tableName + " WHERE uname=$1";
-        return db.execSingleQuery(query, [dataObj.uname]);
-    }
-
-    create(dataObj) {
-        if(!dataObj.uname) {
-            return Promise.reject("Must include valid username.");
-        }
-
-        return bcrypt.hash(dataObj.password, saltRounds).then((hash) => {
-            const hashedDataObj = Object.assign({},dataObj);
-            hashedDataObj.password = hash;
-            let query = squel.insert()
-            .into(this.tableName)
-            .setFieldsRows([hashedDataObj]).toString();
-            return super.insert(query, hashedDataObj, "Error creating user: user already exists.");
-        });
-    }
-
-    verify(dataObj) {
-        if(!dataObj.uname) {
-            return Promise.reject("Bad username");
-        }
-        
-        let query = "SELECT * FROM " + this.tableName + " WHERE uname=$1";
-        return db.execSingleQuery(query, [dataObj.uname]).then((result) => {
-        
-        result = result.rows;
-        if(result.length!=1) {
-          return Promise.reject("User Doesn't Exist");
-        }
-        result = result[0];
-
-        return bcrypt.compare(dataObj.password, result.password).then((res) => {
-          if(res) {
-                return {
-                  uname:result.uname,
-                  admin:result.admin,
-                  id:result.id
-                };
-            }
-            else
-                return Promise.reject("Incorrect Password");
-            });
-        });
-    }
-    
-    search(names, filter) {
-      names = QueryGenerator.transformQueryArr(names);
-      let query = squel.select()
-      .from(this.tableName)
-      .join("plant_mgr", null, this.tableName + '.id = plant_mgr.user_id')
-      .field("*, COUNT(*) OVER() as row_count");
-
-      const queryGen = new QueryGenerator(query);
-      queryGen.chainAndFilter(names, "uname LIKE ?");
-      let queryStr = filter.applyFilter(queryGen.getQuery()).toString();
-      //logger.debug(queryStr);
-      return db.execSingleQuery(queryStr, [])
-        .then((result) => {
-          console.log(queryStr);
-        });
+  constructor() {
+    super();
+    this.tableName = "users";
   }
 
-    getUser(dataObj) {
-      if (!dataObj.uname) {
-        return Promise.reject("Bad username");
-      }
-
-      let query = "SELECT * FROM " + this.tableName + " WHERE uname=$1";
-      return db.execSingleQuery(query, [dataObj.uname]).then((result) => {
-        result = result.rows;
-        if (result.length != 1) {
-          return Promise.reject("User Doesn't Exist");
-        }
-        result = result[0];
-        return result;
-      });
+  checkExisting(dataObj) {
+    if (!dataObj.uname) {
+      return Promise.reject("Bad Username.");
     }
 
-    update(dataObj, oldPrimaryKey) {
-      if(dataObj.password){
-        return bcrypt.hash(dataObj.password, saltRounds).then((hash) => {
-            const hashedDataObj = Object.assign({},dataObj);
-            hashedDataObj.password = hash;
-            return super.change(hashedDataObj, oldPrimaryKey, "id");
-        });
-      } else {
-        return super.change(dataObj, oldPrimaryKey, "id");
-      }
+    let query = "SELECT COUNT(*) FROM " + this.tableName + " WHERE uname=$1";
+    return db.execSingleQuery(query, [dataObj.uname]);
+  }
+
+  create(dataObj) {
+    if (!dataObj.uname) {
+      return Promise.reject("Must include valid username.");
     }
 
-    remove(id) {
-        // if(!id) {
-        //     return Promise.reject("Bad User ID.");
-        // }
-        // return db.execSingleQuery("DELETE FROM " + this.tableName + " WHERE id = $1", [id]);
-        //if SKUs have this product line, we should not be able to remove it
-        return db.execSingleQuery("SELECT * FROM manufacturing_goal WHERE user_id=$1 LIMIT 1", [id])
-        .then((res)=> {
-            if(res.rows.length > 0) {
-                return Promise.reject("Cannot remove " + id + ": Manufacturing Goals are owned by user.");
+    return bcrypt.hash(dataObj.password, saltRounds).then((hash) => {
+      const hashedDataObj = Object.assign({}, dataObj);
+      hashedDataObj.password = hash;
+      let query = squel.insert()
+        .into(this.tableName)
+        .setFieldsRows([hashedDataObj]).toString();
+      return super.insert(query, hashedDataObj, "Error creating user: user already exists.");
+    });
+  }
+
+  verify(dataObj) {
+    if (!dataObj.uname) {
+      return Promise.reject("Bad username");
+    }
+
+    let query = "SELECT * FROM " + this.tableName + " WHERE uname=$1";
+    return db.execSingleQuery(query, [dataObj.uname]).then((result) => {
+
+      result = result.rows;
+      if (result.length != 1) {
+        return Promise.reject("User Doesn't Exist");
+      }
+      result = result[0];
+
+      return bcrypt.compare(dataObj.password, result.password).then((res) => {
+        if (res) {
+          delete result.password;
+          getPlantsManagedBy(result.id).then((lines) => {
+            lines = lines.rows;
+            if (!lines) {
+              return result;
             }
-            return res;
-        })
-        .then((res)=> {
-            //verify that no SKUs depend on this product line
-            return db.execSingleQuery("DELETE FROM " + this.tableName + " WHERE id=$1", [id]);
-        });
+            lines = lines.reduce((ret, cur) => {
+              return ret.push(cur.manline_id);
+            }, []);
+            return {
+              ...result,
+              manlines: lines
+            }
+          })
+          // return {
+          //   uname: result.uname,
+          //   admin: result.admin,
+          //   id: result.id
+          // };
+        }
+        else
+          return Promise.reject("Incorrect Password");
+      });
+    });
+  }
+
+  search(names, filter) {
+    names = QueryGenerator.transformQueryArr(names);
+    let query = squel.select()
+      .from(this.tableName)
+      .field("*, COUNT(*) OVER() as row_count");
+
+    const queryGen = new QueryGenerator(query);
+    queryGen.chainAndFilter(names, "uname LIKE ?");
+    let queryStr = filter.applyFilter(queryGen.getQuery()).toString();
+    //logger.debug(queryStr);
+    return db.execSingleQuery(queryStr, []);
+  }
+
+  getPlantsManagedBy(id) {
+    let query = squel.select()
+      .from("plant_mgr")
+      .where("user_id = ?", id)
+      .toString();
+    return db.execSingleQuery(query, []);
+  }
+
+  getUser(dataObj) {
+    if (!dataObj.uname) {
+      return Promise.reject("Bad username");
     }
+
+    let query = "SELECT * FROM " + this.tableName + " WHERE uname=$1";
+    return db.execSingleQuery(query, [dataObj.uname]).then((result) => {
+      result = result.rows;
+      if (result.length != 1) {
+        return Promise.reject("User Doesn't Exist");
+      }
+      result = result[0];
+      delete result.password;
+      getPlantsManagedBy(result.id).then((lines) => {
+        lines = lines.rows;
+        if (!lines) {
+          return result;
+        }
+        lines = lines.reduce((ret, cur) => {
+          return ret.push(cur.manline_id);
+        }, []);
+        return {
+          ...result,
+          manlines: lines
+        }
+      });
+    });
+  }
+
+  update(dataObj, oldPrimaryKey) {
+    if (dataObj.password) {
+      return bcrypt.hash(dataObj.password, saltRounds).then((hash) => {
+        const hashedDataObj = Object.assign({}, dataObj);
+        hashedDataObj.password = hash;
+        return super.change(hashedDataObj, oldPrimaryKey, "id");
+      });
+    } else {
+      return super.change(dataObj, oldPrimaryKey, "id");
+    }
+  }
+
+  remove(id) {
+    // if(!id) {
+    //     return Promise.reject("Bad User ID.");
+    // }
+    // return db.execSingleQuery("DELETE FROM " + this.tableName + " WHERE id = $1", [id]);
+    //if SKUs have this product line, we should not be able to remove it
+    return db.execSingleQuery("SELECT * FROM manufacturing_goal WHERE user_id=$1 LIMIT 1", [id])
+      .then((res) => {
+        if (res.rows.length > 0) {
+          return Promise.reject("Cannot remove " + id + ": Manufacturing Goals are owned by user.");
+        }
+        return res;
+      })
+      .then((res) => {
+        //verify that no SKUs depend on this product line
+        return db.execSingleQuery("DELETE FROM " + this.tableName + " WHERE id=$1", [id]);
+      });
+  }
 }
 
 module.exports = Users;
--- routes/users.js
+++ routes/users.js
@@ -55,11 +55,7 @@ router.post('/', function(req, res, next) {
 
     users.verify(req.body)
     .then((result) => {
-        console.log("SETTING SESSION");
-        req.session.user = result.uname;
-        req.session.admin = result.admin;
-        req.session.user_id = result.id;
-        console.log(req.session);
+        console.log(result);
         res.status(200).json(result);
     })
     .catch((err) => {
