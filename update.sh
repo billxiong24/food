c582c62568cfcfb5e73f76f96c483b38e4e93447
rsh25@duke.edu <rsh25@duke.edu>
2019-04-15 00:21:40 -0400

    added scheduler changes

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@ sudo -u vcm git pull
 # NODE=$(sudo netstat -lp > update.py)
 # sudo kill "$NODE"
 # sudo node app.js
-netstat -lp | python update.py
+NODE=$(netstat -lp | python update.py)
+echo $NODE
