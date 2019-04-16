f64021eae86fc7e57d52a99bd1a9c19ea9fd9724
rsh25@duke.edu <rsh25@duke.edu>
2019-04-16 00:51:31 -0400

    added scheduler changes

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import fileinput
 import sys
 
 for line in fileinput.input():
-        print(line)
+    print(line)
     if "8000" in line:
         line_parts = line.split("LISTEN")
         last_part = line_parts[-1]
